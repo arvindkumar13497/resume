eea05c719a403196505354ac8ad116be5d0ed149
arvindkumar13497 <arvindkumar13497@gmail.com>
2026-06-29 19:15:58 +0530

    hghghg

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useEffect, useState } from 'react'
 import './App.css'
 import { PortfolioPage } from './pages/PortfolioPage'
 
 function App() {
-  const [theme, setTheme] = useState<'dark' | 'light'>('dark')
 
-  useEffect(() => {
-    document.documentElement.dataset.theme = theme
-  }, [theme])
 
-  return <PortfolioPage theme={theme} toggleTheme={() => setTheme((value) => (value === 'dark' ? 'light' : 'dark'))} />
+
+  return <PortfolioPage />
 }
 
 export default App
